26ab06b84b59ccfc8d4acb3c478da56b227a7c7a
Om Prakash Dhiraj <om.dhiraj@ad.infosys.com>
2023-07-04 16:02:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+//since we are using es6 so we change type in package.json from common to module
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+import cors from "cors";
+
+//configure env
+dotenv.config();
+
+//database config
+connectDB();
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+//rest api
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce APP</h1>");
+});
+
+//Port
+const PORT = process.env.PORT || 8080;
+
+//run listen
+
+app.listen(PORT, () => {
+  console.log(
+    `server running on ${process.env.DEV_MODE}  mode on Port ${PORT}`
+  );
+});
